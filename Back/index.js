f632e4ae79499bd3ecc471a7fec7d68d39691192
Neovhack <nnnicoboland@hotmail.com>
2023-04-01 16:45:56 -0300

    UPDATE PORT

--- Back/index.js
+++ Back/index.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
 
-const PORT = process.env.PGPORT || 3001;
+const PORT = process.env.PORT || 3001;
 
 
 // Syncing all the models at once.
